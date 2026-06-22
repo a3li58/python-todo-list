bb1328732a5c2d0d9d2ceb1ec828383d1190d25b
Ali Ali <ali.ali@my.bdsc.school.nz>
2026-06-22 12:41:42 +1200

    Complete version 3

--- to do v3.py
+++ to do v3.py
@@ -34,4 +34,72 @@ while True:
 
             save_tasks()
             easygui.msgbox("Task added successfully!")
+    elif choice == "View Tasks":
+        if len(tasks) == 0:
+            easygui.msgbox("No tasks found.")
+        else:
+            task_text = ""
+
+            for i, task in enumerate(tasks, start=1):
+                status = "✓" if task["done"] else "✗"
+                task_text += f"{i}. {task['task']} | {task['time']} | {status}\n"
+
+            easygui.textbox("Your Tasks", "Tasks", task_text)
+
+   
+    elif choice == "Complete Task":
+        if len(tasks) == 0:
+            easygui.msgbox("No tasks available.")
+        else:
+            task_text = ""
+
+            for i, task in enumerate(tasks, start=1):
+                status = "✓" if task["done"] else "✗"
+                task_text += f"{i}. {task['task']} | {task['time']} | {status}\n"
+
+            task_num = easygui.enterbox(
+                "Tasks:\n\n" + task_text + "\nEnter task number to complete:"
+            )
+
+            if task_num and task_num.isdigit():
+                task_num = int(task_num)
+
+                if 1 <= task_num <= len(tasks):
+                    tasks[task_num - 1]["done"] = True
+                    save_tasks()
+                    easygui.msgbox("Task completed!")
+                else:
+                    easygui.msgbox("Invalid task number.")
+            else:
+                easygui.msgbox("Please enter a valid number.")
+
+    
+    elif choice == "Delete Task":
+        if len(tasks) == 0:
+            easygui.msgbox("No tasks to delete.")
+        else:
+            task_text = ""
+
+            for i, task in enumerate(tasks, start=1):
+                status = "✓" if task["done"] else "✗"
+                task_text += f"{i}. {task['task']} | {task['time']} | {status}\n"
+
+            task_num = easygui.enterbox(
+                "Tasks:\n\n" + task_text + "\nEnter task number to delete:"
+            )
+
+            if task_num and task_num.isdigit():
+                task_num = int(task_num)
+
+                if 1 <= task_num <= len(tasks):
+                    deleted_task = tasks.pop(task_num - 1)
+                    save_tasks()
+                    easygui.msgbox(f"{deleted_task['task']} deleted.")
+                else:
+                    easygui.msgbox("Invalid task number.")
+            else:
+                easygui.msgbox("Please enter a valid number.")
+    else:
+        easygui.msgbox("Goodbye!")
+        break
 
